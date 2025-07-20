108835e8844b8f3ca6196d01b049f3d8dc7fb59a
savio04 <savio27.05.2001@gmail.com>
2025-07-19 21:45:11 -0300

    Fix: Add build script

--- backend/src/shared/infra/postgres/migrations/002_create_devices_table.sql
+++ backend/src/shared/infra/postgres/migrations/002_create_devices_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE devices (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   status VARCHAR(50) NOT NULL,
   secret VARCHAR(255) NOT NULL,
-  user_id VARCHAR(255),
+  user_id UUID,
   CONSTRAINT fk_user
     FOREIGN KEY (user_id)
     REFERENCES users(id)
